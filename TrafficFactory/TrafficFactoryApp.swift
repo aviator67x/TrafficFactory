4e469738ba846caa3abc612f8a6382d5ee8d0007
Andrew Kasilov <aviator67x@gmail.com>
2024-05-30 11:32:09 +0200

    initial

---
+++ TrafficFactory/TrafficFactoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficFactoryApp.swift
+//  TrafficFactory
+//
+//  Created by Andrew Kasilov on 30.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct TrafficFactoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
